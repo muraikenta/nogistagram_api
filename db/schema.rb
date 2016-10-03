53b547b64550e68933c090185a2333444d709bf6
村井謙太 <muraikenta0507@gmail.com>
2016-10-03 20:51:18 +0900

    add user_id to Post

--- db/schema.rb
+++ db/schema.rb
@@ -11,28 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161003111756) do
-
-  create_table "posts", force: :cascade do |t|
-    t.string   "image_url"
-    t.text     "body"
-    t.float    "latitude"
-    t.float    "longitude"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "unique_name"
-    t.string   "email"
-    t.text     "introduction"
-    t.string   "website"
-    t.integer  "gender"
-    t.string   "image_url"
-    t.integer  "privacy_type"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
