975a3a1900b8c3718e55c9de7b0cdf67dca0a1e9
村井謙太 <muraikenta0507@gmail.com>
2016-10-03 21:04:29 +0900

    generate model Follow and add validation

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,42 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20161003115533) do
+
+  create_table "follows", force: :cascade do |t|
+    t.integer  "from_user_id"
+    t.integer  "to_user_id"
+    t.integer  "state",        default: 1
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
+
+  add_index "follows", ["from_user_id"], name: "index_follows_on_from_user_id"
+  add_index "follows", ["to_user_id"], name: "index_follows_on_to_user_id"
+
+  create_table "posts", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "image_url"
+    t.text     "body"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "unique_name"
+    t.string   "email"
+    t.text     "introduction"
+    t.string   "website"
+    t.integer  "gender"
+    t.string   "image_url"
+    t.integer  "privacy_type", default: 0
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
 
 end
