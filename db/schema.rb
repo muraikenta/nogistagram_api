232982142cf3441ba6a566dcfbad63804fd33bae
村井謙太 <muraikenta0507@gmail.com>
2016-10-03 21:06:44 +0900

    generate model Like

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161003115533) do
+ActiveRecord::Schema.define(version: 20161003120551) do
 
   create_table "follows", force: :cascade do |t|
     t.integer  "from_user_id"
@@ -23,6 +23,16 @@ ActiveRecord::Schema.define(version: 20161003115533) do
 
   add_index "follows", ["from_user_id"], name: "index_follows_on_from_user_id"
   add_index "follows", ["to_user_id"], name: "index_follows_on_to_user_id"
+
+  create_table "likes", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "likes", ["post_id"], name: "index_likes_on_post_id"
+  add_index "likes", ["user_id"], name: "index_likes_on_user_id"
 
   create_table "posts", force: :cascade do |t|
     t.integer  "user_id"
