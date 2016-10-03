c7b3e9a04e324367a6893c2068b25dec03b89c1d
村井謙太 <muraikenta0507@gmail.com>
2016-10-03 20:38:08 +0900

    annotation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  image_url  :string
+#  body       :text
+#  latitude   :float
+#  longitude  :float
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Post < ActiveRecord::Base
 end
