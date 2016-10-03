de11defa9586318fc7c98e9de1016eab3b17aff9
村井謙太 <muraikenta0507@gmail.com>
2016-10-03 21:04:12 +0900

    add user privacy_state default

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@
 #  website      :string
 #  gender       :integer
 #  image_url    :string
-#  privacy_type :integer
+#  privacy_type :integer          default(0)
 #  created_at   :datetime         not null
 #  updated_at   :datetime         not null
 #
