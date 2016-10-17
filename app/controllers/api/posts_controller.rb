86bde341078f407e178fbbb284fd8868e6fffcf8
shohohoh <so10ve.q0fsu0u@gmail.com>
2016-10-17 19:16:55 +0900

    fux comment by muraikenta

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,12 +1,6 @@
 class Api::PostsController < Api::BaseController
-  before_action :authenticate_user!
-
   def index
-    if current_user
-      posts = current_user.timeline_posts
-      render json: posts
-    else
-      render_not_found
-    end
+    posts = current_user.timeline_posts
+    render json: posts
   end
 end
