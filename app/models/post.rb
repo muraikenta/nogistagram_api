ac15cbdce5535ef2695b309fffd68921c4f99a7f
shohohoh <so10ve.q0fsu0u@gmail.com>
2016-10-23 16:58:41 +0900

    Jbuilder入れた

--- app/models/post.rb
+++ app/models/post.rb
@@ -15,6 +15,7 @@
 class Post < ActiveRecord::Base
   # associations
   belongs_to :user
+  has_many :likes, dependent: :destroy
 
   # validations
   validates :image_url, presence: true, uniqueness: true
@@ -23,5 +24,18 @@ class Post < ActiveRecord::Base
     file_name = Time.zone.now.to_i.to_s + rand(1_000_000).to_s + '.png'
     File.open("public/post_images/#{file_name}", 'wb') { |f| f.write(binary) }
     self.image_url = "http://localhost:3002/post_images/#{file_name}"
+  end
+
+  def to_builder(opts = {})
+    user = opts[:user]
+    Jbuilder.new do |post|
+      post.call(self, :id, :user_id, :image_url, :body, :created_at)
+      post.user self.user
+      post.is_liked self.liked_from?(user)
+    end
+  end
+
+  def liked_from?(user)
+    self.likes.find_by(user: user).present?
   end
 end
