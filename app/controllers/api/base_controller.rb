251b95eb36d76af838a06ae7882c629ce465da1f
村井謙太 <muraikenta0507@gmail.com>
2016-10-23 16:47:41 +0900

    apiのエラーをlogに出すように

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -6,6 +6,25 @@ class Api::BaseController < ApplicationController
   rescue_from Exception, with: :render_error
   rescue_from ActionView::MissingTemplate, with: :render_not_found
 
+  rescue_from Exception, with: :render_500
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
+  rescue_from ActionController::RoutingError, with: :render_404
+
+  # routes.rbで使用
+  def routing_error
+    fail ActionController::RoutingError, params[:path]
+  end
+
+  def render_404(e = nil)
+    logger.info "Rendering 404 with exception: #{e.message}" if e
+    render json: { error: '404 error' }, status: 404
+  end
+
+  def render_500(e = nil)
+    logger.info "Rendering 500 with exception: #{e.message}" if e
+    render json: { error: '500 error' }, status: 500
+  end
+
   def render_success(message = '')
     @_success = true
     render(
